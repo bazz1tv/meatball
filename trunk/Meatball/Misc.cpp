e0b27aa3db3a2d134611bf9fc93c96322b85d73f
mikebazz <mikebazz@5b324fe7-80eb-46bd-bd03-aee906d08faa>
2013-01-15 11:59:00 +0000

    forgot to add some files to the big big commit

---
+++ trunk/Meatball/Misc.cpp
@@ -0,0 +1,81 @@
+#include "Misc.h"
+
+void UniversalEventHandler(SDL_Event *event)
+{
+	switch(event->type)
+	{
+		case SDL_VIDEORESIZE:
+		{
+			
+			//Resize the screen
+			Screen = SDL_SetVideoMode( event->resize.w, event->resize.h, pGameSettings->Screen_Bpp, MEATBALL_CUSTOM_FLAGS );
+			//If there's an error
+			if( Screen == NULL )
+			{
+				printf ("OOPS");
+				//windowOK = false;
+				return;
+			}
+			break;
+		}
+		case SDL_VIDEOEXPOSE:
+		{
+			//printf ("OOPS");
+			//Update the screen
+			if( SDL_Flip( Screen ) == -1 )
+			{
+				//If there's an error
+				//windowOK = false;
+				printf ("OOPS");
+				return;
+			}
+			break;
+		}
+		case SDL_QUIT:
+		{
+			mode = MODE_QUIT;
+			break;
+		}
+		case SDL_KEYDOWN:
+		{
+			if( event->key.keysym.sym == SDLK_RETURN && ( event->key.keysym.mod & KMOD_ALT ) ) 
+			{
+					SDL_ToggleFS(Screen);
+			}
+		}
+
+	}
+}
+
+
+/// Queries the Screen if it's set to Full of Not
+/// @return 0 if windowed, 1 if fullscreen
+int IsFullScreen(SDL_Surface *surface) 
+{ 
+    if (surface->flags & SDL_FULLSCREEN) return 1; // return true if surface is fullscreen 
+    return 0; // Return false if surface is windowed 
+} 
+
+/// Toggles On/Off FullScreen
+int SDL_ToggleFS(SDL_Surface *surface) 
+{ 
+    Uint32 flags = surface->flags; // Get the video surface flags 
+    
+    if (IsFullScreen(surface)) 
+    { 
+        // Swith to WINDOWED mode 
+        if ((surface = SDL_SetVideoMode(MEATBALL_WIN_SETTINGS, (flags | SDL_RESIZABLE) & ~SDL_FULLSCREEN)) == NULL) return 0; 
+        return 1; 
+    } 
+    
+    // Swith to FULLSCREEN mode 
+    if ((surface = SDL_SetVideoMode(MEATBALL_FULLSCREEN_SETTINGS, (flags|SDL_FULLSCREEN))) == NULL) return 0; 
+    return 1; 
+} 
+
+/// Check for Collision with the Mouse
+/// @returns true for collision
+bool MouseCollidesWith(SDL_Rect *Crect)
+{
+	return pMouse->CollisonCheck( Crect );
+}
---
+++ trunk/Meatball/include/OptionsMenu.h
@@ -0,0 +1,42 @@
+#ifndef __OPTIONSMENU_H__
+#define __OPTIONSMENU_H__
+
+#include "Globals.h"
+/*
+/// @ingroup Options_Screen_Statuses
+// @{
+#define STATUS_BROWSING 0
+#define STATUS_SLIDING	1
+#define STATUS_EDITING	2
+// @}
+
+struct TextObject
+{
+	SDL_Surface *surface;
+	SDL_Rect rect;
+};
+
+class OptionsMenu
+{
+	Uint8 status;
+	// StateHandler InputEngine
+	
+	OptionsMenu();
+	~OptionsMenu();
+	BlinkCursor();
+	LiveInput();
+	Slide(Slider *slider);
+	Keyboard_Joypad_Oneshot();
+
+	cBasicSprite *ptr; // This is how you store sprites in the class
+	SDL_Surface *cursor;
+	SDL_Event event;
+	TTF_Font *font;
+
+	TextObject Exit, Music_Volume, Sound_Volume;
+	double ttDrawCur;
+	bool DrawCur;
+};
+*/
+
+#endif
