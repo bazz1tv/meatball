dfc187cd55f7d003ddd523650eb91eecccbdda6c
Bazz <2224787+bazzinotti@users.noreply.github.com>
2013-02-11 03:31:38 +0000

    derp

--- Meatball/include/defines.h
+++ Meatball/include/defines.h
@@ -15,8 +15,8 @@
 #define JUMP_KEYS_HELD		(keys[SDL_GetScancodeFromKey(pGameSettings->Key_jump)]) // || keys[SDLK_RALT] || keys[SDLK_SEMICOLON] || keys[SDLK_PERIOD] || keys[SDLK_m])
 #define MOVELEFT_KEYS_HELD	(keys[SDL_GetScancodeFromKey(pGameSettings->Key_walk_left)]) //  || keys[SDLK_a])
 #define MOVERIGHT_KEYS_HELD (keys[SDL_GetScancodeFromKey(pGameSettings->Key_walk_right)]) //|| keys[SDLK_d])
-#define JUMP_KEYS_TRIGGERED (event.key.keysym.sym == (pGameSettings->Key_jump)) // || event.key.keysym.sym == (SDLK_RALT) ||event.key.keysym.sym == SDLK_SEMICOLON || event.key.keysym.sym == SDLK_PERIOD || event.key.keysym.sym == SDLK_m)
-#define FIRE_KEY_TRIGGERED	(event.key.keysym.sym == (pGameSettings->Key_shoot_primary))
+#define JUMP_KEYS_TRIGGERED (event->key.keysym.sym == (pGameSettings->Key_jump)) // || event.key.keysym.sym == (SDLK_RALT) ||event.key.keysym.sym == SDLK_SEMICOLON || event.key.keysym.sym == SDLK_PERIOD || event.key.keysym.sym == SDLK_m)
+#define FIRE_KEY_TRIGGERED	(event->key.keysym.sym == (pGameSettings->Key_shoot_primary))
 #define FIRE_KEY_HELD	(keys[SDL_GetScancodeFromKey(pGameSettings->Key_shoot_primary)])
 
 // The Directorys
